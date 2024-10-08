f396bb207b62ca90fa01afc19f2ccb0a45aaa520
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-08 17:02:59 +0200

    version

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Set executable permissions for the ChromeDriver
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Set executable permissions for ChromeDriver
 chmod +x script/chromedriver
 
+# Check Chrome and ChromeDriver versions
 google-chrome --version
+script/chromedriver --version
