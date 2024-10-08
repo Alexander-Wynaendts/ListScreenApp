72b36ec85df02a518f5fed857b6ab9ba5cb85c1e
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-08 17:10:15 +0200

    chrome_dowload

--- render-build.sh
+++ render-build.sh
@@ -1,17 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Install Google Chrome manually
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -x google-chrome-stable_current_amd64.deb /tmp/google-chrome
-export PATH=/tmp/google-chrome/opt/google/chrome:$PATH
+STORAGE_DIR=/opt/render/project/.render
 
-# Get Chrome version and download the matching ChromeDriver
-CHROME_VERSION=$(/tmp/google-chrome/opt/google/chrome/google-chrome --version | grep -oP '\d{2,3}' | head -1)
+# Download Chrome if not cached
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src
+else
+  echo "...Using Chrome from cache"
+fi
+
+# Install ChromeDriver
+CHROME_VERSION=$($STORAGE_DIR/chrome/opt/google/chrome/google-chrome --version | grep -oP '\d{2,3}' | head -1)
 DRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d script
 chmod +x script/chromedriver
 
 # Print versions
-/tmp/google-chrome/opt/google/chrome/google-chrome --version
+$STORAGE_DIR/chrome/opt/google/chrome/google-chrome --version
 script/chromedriver --version
