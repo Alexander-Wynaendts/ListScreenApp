9d0c1c338338615464ac5d1afb54c0d25081432e
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-08 17:14:24 +0200

    chrome

--- render-build.sh
+++ render-build.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-# Download Chrome if not cached
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src
+  cd $HOME/project/src # Return to original directory
 else
   echo "...Using Chrome from cache"
 fi
 
-# Install ChromeDriver
+# Add Chrome to the PATH
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# Download ChromeDriver
 CHROME_VERSION=$($STORAGE_DIR/chrome/opt/google/chrome/google-chrome --version | grep -oP '\d{2,3}' | head -1)
 DRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip
