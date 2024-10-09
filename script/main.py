517f0ce7bedba2faa03eb30fda2852097c391480
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-09 09:24:05 +0200

    Beta Version

--- script/main.py
+++ script/main.py
@@ -11,9 +11,6 @@ def main(startup_data):
     # Appeler la fonction de data_formatting pour traiter les données
     startup_data = data_formatting(startup_data)
 
-    # Keep only the first 5 rows for processing
-    startup_data = startup_data[:2]
-
     # Check if the processed data is a DataFrame
     if not isinstance(startup_data, pd.DataFrame):
         return None  # Return None in case of an error
