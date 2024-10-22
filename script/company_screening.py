86ff898f6c457cffad52a6fcd4e267d945c252c6
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-22 15:23:14 +0200

    first import

--- script/company_screening.py
+++ script/company_screening.py
@@ -1,7 +1,5 @@
 from .website_screening import website_screen_process
 from .website_analysis import website_analysis_process
-from .import_affinity import import_affinity
-
 
 def company_screening(company_info):
 
@@ -14,8 +12,5 @@ def company_screening(company_info):
     # Perform website analysis (now expects a dictionary)
     company_info = website_analysis_process(company_info)
 
-    # Import to Affinity
-    import_affinity(company_info)
-
     # Return the final processed result as a success message
-    return f"Company '{company_info['Name']}' was screened, analyzed, and imported successfully"
+    return company_info
