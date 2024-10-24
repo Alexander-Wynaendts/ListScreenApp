f4c85e3edff1a4252b4d168db8b5f136b3c90478
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-24 17:07:40 +0200

    gmail export

--- script/add_people_to_affinity.py
+++ script/add_people_to_affinity.py
@@ -47,6 +47,6 @@ def add_people_to_affinity(first_name, last_name, email, website_url):
         data = {"first_name": first_name, "last_name": last_name, "emails": [email], "organization_ids": [organization_id]}
         response  = requests.put(url, auth=HTTPBasicAuth('', api_key), json=data)
         print("ADDED", email)
-        print(response)
+        print(response.json())
 
     return
