20bc38761f5f518a924426a1d751f3a7bbd5d66f
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-29 15:04:10 +0100

    full test

--- app.py
+++ app.py
@@ -208,8 +208,6 @@ def lemlist_webhook_contacted():
     if request.method == 'POST':
         data = request.json
 
-        print(data)
-
         website_url = data.get("companyDomain", "")
         company_status = "Contacted"
 
