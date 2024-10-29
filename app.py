47f1fa5678597910049f8a5406ddf55774282436
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-29 18:00:31 +0100

    time sleep

--- app.py
+++ app.py
@@ -168,6 +168,7 @@ def formulair_webhook():
 
         # Add company to Affinity using name and website_url
         add_company_to_affinity(name, website_url)
+        time.sleep(10)
 
         # Iterate over the first_names, last_names, and emails lists
         for first_name, last_name, email in zip(first_names, last_names, emails):
