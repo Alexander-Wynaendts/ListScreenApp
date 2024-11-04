4082183b16ab1693852c275fdb3a984d479803b0
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-11-04 12:20:46 +0100

    gmail comments

--- script/gmail_comments.py
+++ script/gmail_comments.py
@@ -1,8 +1,7 @@
 import re
 
-def gmail_comments(data):
+def extract_original_sender_and_comment(data):
     # Retrieve the original sender's email from the forwarded message in the body
-    # This assumes the original sender is the one farthest back in the plainBody text
     plain_body = data.get('plainBody', '')
     original_sender_match = re.search(r'From: .*? <(.*?)>', plain_body)
     if original_sender_match:
@@ -13,9 +12,8 @@ def gmail_comments(data):
         original_email = None
         company_email_domain = None
 
-    # Retrieve the first email content (latest comment) in the thread
-    # Assuming it's the first line of plainBody text
-    last_comment_match = re.search(r'^.*$', plain_body, re.MULTILINE)
+    # Retrieve the first email content (last comment) in the thread up to '\r\n\r\n>'
+    last_comment_match = re.search(r'^(.*?)(?=\r\n\r\n>)', plain_body, re.DOTALL)
     last_comment = last_comment_match.group(0) if last_comment_match else None
 
     return company_email_domain, last_comment
